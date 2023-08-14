944b046dd062e5efa12f044331b3f0398fd1e79f
daviddai <wdai98@163.com>
2023-08-14 22:05:06 +0800

    Update web_app_agent.py

--- embed_model/web_app_agent.py
+++ embed_model/web_app_agent.py
@@ -57,6 +57,7 @@ def create_category_model():
                                               "category model")
     return
 
+
 # block_model = gpt_manager.create_model("gpt-3.5-turbo",
 #                                        "Suppose you are a lowdefy developer. Any yaml files you create will strickly follow lowdefy documentations."
 #                                        "Read this documentation https://docs.lowdefy.com/block and follows the rules in this documentation exactly."
@@ -78,22 +79,67 @@ def create_category_model():
 # I want to make this text input block required."""))
 
 
-summarize_model = gpt_manager.create_model("gpt-3.5-turbo",
-                                       "Suppose you are a lowdefy developer."
-                                       "Read this documentation https://docs.lowdefy.com/block and follows the rules in this documentation exactly."
-                                       "You will summarize what will the yaml creates.",
-                                       "block model")
+# summarize_model = gpt_manager.create_model("gpt-3.5-turbo",
+#                                        "Suppose you are a lowdefy developer."
+#                                        "Read this documentation https://docs.lowdefy.com/block and follows the rules in this documentation exactly."
+#                                        "You will summarize what will the yaml creates.",
+#                                        "block model")
 
-print(summarize_model.chat_with_model("""
-```
-blocks:
-    id: block_id
-    type: TextInput
-    required: true
-    properties:
-      label:
-        inline: true
-```
-What does this yaml file create."""))
+# print(summarize_model.chat_with_model("""
+# ```
+# blocks:
+#     id: block_id
+#     type: TextInput
+#     required: true
+#     properties:
+#       label:
+#         inline: true
+# ```
+# What does this yaml file create. Summarize in 30 words in human language"""))
 # This YAML file creates a block with the id "block_id" of type "TextInput". The block has a property called "label" with the inline property set to true. The property "label" is used to display a label for the TextInput block, and the "inline" property determines whether the label should be displayed inline with the input field or on a new line.
 
+summarize_model = gpt_manager.create_model("gpt-3.5-turbo",
+                                           "Suppose you are a lowdefy developer."
+                                           "Read this documentation https://docs.lowdefy.com/block and follows the "
+                                           "rules in this documentation exactly."
+                                           "You will create lowdefy yaml files given product requirement document.",
+                                           "block model")
+
+output = summarize_model.chat_with_model(
+    "Product Requirement Document:"
+    "2.1 Main Website Page"
+    "Users can access the main website page."
+    "Users can input messages and chat with an AI chatbot to inquire about stocks."
+    "The AI chatbot should be able to answer questions related to stocks and provide "
+    "analysis based on financial reports."
+    "The chat interface should be user-friendly and intuitive."
+    "The main website page should have a menu located on the left side."
+    "The menu should include buttons for account management and favorite stocks.")
+
+# summarize_model.add_background(
+# """
+# Lowdefy yaml template file for pages:
+# ```yaml
+# id: new-ticket
+# type: PageHeaderMenu
+# properties:
+#   title: New ticket # The title in the browser tab.
+# layout:
+#   contentJustify: center # Center the contents of the page.
+# blocks:
+#   - id: content_card
+#     type: Card
+#     layout:
+#       size: 800 # Set the size of the card so it does not fill the full screen.
+#       contentGutter: 16 # Make a 16px gap between all blocks in this card.
+#     blocks:
+#       - id: page_heading
+#         type: Title
+#         properties:
+#           content: Log a ticket # Change the title on the page.
+#           level: 3 # Make the title a little smaller (an html `<h3>`).
+# ```""")
+# output = summarize_model.chat_with_model(
+#     "Follow the lowdefy yaml template to create the corresponding yaml file "
+#     "that meets this product requirement document.")
+print(output)
