92322d29b84b2dd885e32ca929ff8a8c030e2bb2
Xinchun Ran <xinchun@accre.vu>
2023-08-25 00:44:52 -0500

    update the dataset generator

--- solidgpt/diy/llama2_diy_finetune/dataset.py
+++ solidgpt/diy/llama2_diy_finetune/dataset.py
@@ -1,84 +1,67 @@
 import os
-import openai
 import json
 import tiktoken
+from solidgpt.manager.gptmanager import GPTModel  # Import GPTModel from the correct location
 
 # Set your OpenAI API key here
-openai.api_key = ''
+openai.api_key = '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'
 
 MAX_TOKENS_LIMIT = 16000
 
-class GPTModel:
-    def __init__(self, model, temperature=0.5):
-        self.model = model
-        self.temperature = temperature
-        self.messages = []
-        self.last_reply = None
+class GenerateDataset:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        prompt = "Explain the content of a Lowdefy YAML file."  # Define the prompt
+        self.gpt_model = GPTModel(prompt, model_name)
 
-    def generate_completion(self, input_message):
-        self.messages.append({"role": "user", "content": input_message})
-        chat = openai.ChatCompletion.create(
-            model=self.model,
-            messages=self.messages,
-            temperature=self.temperature,
-        )
-        reply = chat.choices[0].message.content
-        self.messages.append({"role": "assistant", "content": reply})
-        return reply
+    def _num_tokens_from_string(self, string: str, encoding_name: str) -> int:
+        encoding = tiktoken.encoding_for_model(encoding_name)
+        num_tokens = len(encoding.encode(string))
+        return num_tokens
 
-def read_file(file_path):
-    with open(file_path, "r") as f:
-        return f.read()
+    def get_files_with_filter(self, root_path: str, regex_filter: str) -> list:
+        filtered_files = []
+        for dirpath, dirnames, filenames in os.walk(root_path):
+            for filename in filenames:
+                if filename.lower().endswith(regex_filter.lower()):
+                    filtered_files.append(os.path.join(dirpath, filename))
+        return filtered_files
 
-def num_tokens_from_string(string: str, encoding_name: str) -> int:
-    encoding = tiktoken.encoding_for_model(encoding_name)
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
+    def generate_dataset(self, files_list):
+        dataset = []
+        for file_path in files_list:
+            with open(file_path, "r") as f:
+                yaml_content = f.read()
 
-def main():
-    root_folder = "./lowdefy_raw_repo/lowdefy-example-survey/"
-
-    files = os.listdir(root_folder)
-    gpt_model = GPTModel("gpt-3.5-turbo-16k")
-
-    combined_results = {}
-    combined_results_readme = {}
-    for file_name in files:
-        if file_name.lower() == "readme.md":
-            file_path = os.path.join(root_folder, file_name)
-            readme_content = read_file(file_path)
-            readme_prompt = f"Generate summary for README:\n{readme_content}"
-            readme_summary = gpt_model.generate_completion(readme_prompt)
-            combined_results_readme[file_name] = {
-                "prompt": readme_prompt,
-                "completion": readme_summary,
-            }
-
-    for file_name in files:
-        if file_name.endswith(".yaml") and "README.md" in combined_results_readme:
-            file_path = os.path.join(root_folder, file_name)
-            yaml_content = read_file(file_path)
-
-            readme_summary = combined_results_readme["README.md"]["completion"]
-
-            yaml_prompt = f"Explain the following lowdefy YAML file's name :\n{file_name} function based on the readme and the annotation in the yaml :\n{readme_summary}\n\nYAML Content:\n{yaml_content}"
+            yaml_prompt = f"Explain the following lowdefy YAML file's content:\n{yaml_content}"
             try:
-                yaml_prompt_tokens = num_tokens_from_string(yaml_prompt, "gpt-3.5-turbo-16k")
+                yaml_prompt_tokens = self._num_tokens_from_string(yaml_prompt, self.model_name)
                 if yaml_prompt_tokens <= MAX_TOKENS_LIMIT:
-                    yaml_explanation = gpt_model.generate_completion(yaml_prompt)
-                    combined_results[file_name] = {"prompt": yaml_explanation, "completion": yaml_content}
+                    yaml_explanation = self.gpt_model.chat_with_model(yaml_prompt)
+                    dataset.append({"prompt": yaml_explanation, "completion": yaml_content})
                 else:
-                    print(f"Skipping {file_name} as it exceeds the token limit.")
+                    print(f"Skipping {file_path} as it exceeds the token limit.")
             except openai.error.OpenAIError as e:
                 print(f"Error generating YAML explanation: {e}")
                 continue
+        return dataset
 
-    # Save the combined results to a JSON file
-    output_json_file = "output_combined_results.json"
-    with open(output_json_file, "w") as f:
-        json.dump(combined_results, f, indent=4)
+def main():
+    model_name = "text-davinci-003"  # Replace with your desired model name
+    root_folder = input("Enter the root folder path containing sub-folders with YAML files: ")
 
-    print(f"Combined results saved to {output_json_file}")
+    dataset_generator = GenerateDataset(model_name)
+
+    yaml_files = dataset_generator.get_files_with_filter(root_folder, ".yaml")
+    dataset = dataset_generator.generate_dataset(yaml_files)
+
+    output_jsonl_file = os.path.join(root_folder, 'localstorage', 'train.jsonl')
+    with open(output_jsonl_file, "w") as f:
+        for item in dataset:
+            json.dump(item, f)
+            f.write("\n")
+
+    print(f"Dataset saved to {output_jsonl_file}")
 
 if __name__ == "__main__":
     main()
