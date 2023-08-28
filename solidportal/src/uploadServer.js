f46661005001c6dceb11994443eee7c2e4c27acc
Liao Hu <65468844+colinlaoaa@users.noreply.github.com>
2023-08-28 17:34:37 -0500

    add read file service to read customize skill create by user. (#22)

--- solidportal/src/uploadServer.js
+++ solidportal/src/uploadServer.js
@@ -2,10 +2,13 @@ const express = require('express');
 const multer = require('multer');
 const app = express();
 const port = 3001;
+const cors = require('cors');
+
+app.use(cors());
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Specify the directory where uploaded files will be saved
+        cb(null, '../../localstorage/user_uploads'); // Specify the directory where uploaded files will be saved
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname); // Use the original filename
@@ -17,6 +20,39 @@ const upload = multer({ storage });
 app.post('/upload', upload.single('file'), (req, res) => {
     // File has been uploaded and saved on the server
     res.status(200).json({ message: 'File uploaded successfully!' });
+});
+
+const fs = require('fs');
+app.use(express.json());
+// Define a route to list files in a folder
+app.get('/listfiles', (req, res) => {
+    const folderPath = '../../localstorage/customizedskilldefinition'; // Change this to your folder path
+
+    // Read the list of files in the folder
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.error('Error listing files:', err);
+            return res.status(500).json({ error: 'Error listing files' });
+        }
+
+        res.json({ files });
+    });
+});
+
+
+// Define a route to read file content
+app.get('/readfile/:filename', (req, res) => {
+    const filename = '../../localstorage/customizedskilldefinition/' + req.params.filename;
+
+    // Read the file content
+    fs.readFile(filename, 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err);
+            return res.status(500).json({ error: 'Error reading file' });
+        }
+
+        res.json({ content: data });
+    });
 });
 
 app.listen(port, () => {
