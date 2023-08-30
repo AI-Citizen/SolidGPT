578b9ab32a0f5c0dfef8f739ad8593acd6e310dc
alexgong14 <71686001+alexgong14@users.noreply.github.com>
2023-08-30 04:56:21 -0700

    add run app step

--- demo/creategraph.py
+++ demo/creategraph.py
@@ -107,11 +107,33 @@ def generate_node_sub_page(node_id: int, input_ids: list[int], output_ids: list[
     return node
 
 
+def generate_node_run_app(node_id: int, input_ids: list[int], output_ids: list[int]):
+    # write hld skill
+    skill: WorkSkill = RunApp()
+    skill.init_config(
+        [
+            {
+                "param_path": "",
+                "loading_method": "SkillInputLoadingMethod.LOAD_FROM_OUTPUT_ID",
+                "load_from_output_id": input_ids[0]
+            },
+        ],
+        [
+            {
+                "id": output_ids[0]
+            }
+        ])
+    agent: WorkAgent = AgentSoftwareDeveloper(skill)
+    node: WorkNode = WorkNode(node_id, agent)
+    return node
+
+
 app = WorkGraph()
 app.add_node(generate_node_prd(0, [-1], [0]))
 app.add_node(generate_node_hld(1, [0], [1]))
 app.add_node(generate_node_kanban(2, [1], [2]))
 app.add_node(generate_node_main_page(3, [2], [3]))
 app.add_node(generate_node_sub_page(4, [2], [4]))
+app.add_node(generate_node_run_app(5, [3], [5]))
 app.init_node_dependencies()
 app.save_data("workspace/config/config_data.json")
--- solidgpt/workagent/agents/agent_softwaredeveloper.py
+++ solidgpt/workagent/agents/agent_softwaredeveloper.py
@@ -5,6 +5,6 @@ class AgentSoftwareDeveloper(WorkAgent):
     def __init__(self, skill: WorkSkill):
         super()._agent_setup(
             name=AGENT_NAME_SOFTWARE_DEVELOPER,
-            skills_available=[SKILL_NAME_DEBUG_CODE, SKILL_NAME_WRITE_CODE],
+            skills_available=[SKILL_NAME_DEBUG_CODE, SKILL_NAME_WRITE_CODE, SKILL_NAME_RUN_APP],
             skill=skill,
             )
