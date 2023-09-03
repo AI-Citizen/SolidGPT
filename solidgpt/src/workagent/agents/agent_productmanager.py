7131144dd65e9d94522591628c9313fe5b493555
alexgong14 <71686001+alexgong14@users.noreply.github.com>
2023-09-02 19:11:46 -0700

    delete notion skill name

--- solidgpt/src/workagent/agents/agent_productmanager.py
+++ solidgpt/src/workagent/agents/agent_productmanager.py
@@ -6,6 +6,6 @@ class AgentProductManager(WorkAgent):
     def __init__(self, skill: WorkSkill):
         super()._agent_setup(
             name=AGENT_NAME_PRODUCT_MANAGER,
-            skills_available=[SKILL_NAME_WRITE_PRODUCT_REQUIREMENTS_DOCUMENTATION, SKILL_NAME_USE_NOTION],
+            skills_available=[SKILL_NAME_WRITE_PRODUCT_REQUIREMENTS_DOCUMENTATION],
             skill=skill,
             )
