efa447a322b6cf4480a096b3d1a6a97f34e1531b
Xinchun Ran <xinchun@accre.vu>
2023-10-18 22:41:35 -0500

    add llama prompt builder

--- solidgpt/src/manager/promptresource.py
+++ solidgpt/src/manager/promptresource.py
@@ -1,3 +1,4 @@
+
 PRODUCT_MANAGER_PRD_OUTPUT_TEMPLATE = f'''Base on the information help me generate Markdown PRD follow this format. 
 Here is the output template and explain each sections mean. Always output with this template in Markdown format.
 
@@ -1183,3 +1184,35 @@ def build_custom_skill_gpt_prompt(role_assumption: str, instruction: str, princi
     Here are principles you need to always follow when give the response: {principles}\n\n 
     Here are the prompt and completion examples: {few_shots}
     If no suitable content then response base on your professional knowledge. '''
+
+def llama_v2_prompt(messages):
+    """
+    Convert the messages in list of dictionary format to Llama2 compliant format.
+    """
+    B_INST, E_INST = "[INST]", "[/INST]"
+    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+    BOS, EOS = "<s>", "</s>"
+    DEFAULT_SYSTEM_PROMPT = f"""You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+
+    if messages[0]["role"] != "system":
+        messages = [
+            {
+                "role": "system",
+                "content": DEFAULT_SYSTEM_PROMPT,
+            }
+        ] + messages
+    messages = [
+        {
+            "role": messages[1]["role"],
+            "content": B_SYS + messages[0]["content"] + E_SYS + messages[1]["content"],
+        }
+    ] + messages[2:]
+
+    messages_list = [
+        f"{BOS}{B_INST} {(prompt['content']).strip()} {E_INST} {(answer['content']).strip()} {EOS}"
+        for prompt, answer in zip(messages[::2], messages[1::2])
+    ]
+    messages_list.append(
+        f"{BOS}{B_INST} {(messages[-1]['content']).strip()} {E_INST}")
+
+    return "".join(messages_list)
