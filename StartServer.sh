5d6aaa4b2648be9b286d531ee844e9c196e0a919
Kris Hansen <kris@synctera.com>
2023-10-27 15:27:30 -0400

    fix: detect Darwin and run redis server via brew

--- StartServer.sh
+++ StartServer.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Check if the OS is macOS
+if [[ "$(uname)" == "Darwin" ]]; then
+    # Check if the Redis service is running
+    if ! pgrep redis-server > /dev/null; then
+        # If not, start Redis service using Homebrew
+        brew services start redis
+    fi
+fi
+
+
 service redis-server start
 celery -A solidgpt.src.api.celery_tasks worker --loglevel=info --detach
 uvicorn solidgpt.src.api.api:app --proxy-headers --host 0.0.0.0 --port 8000
